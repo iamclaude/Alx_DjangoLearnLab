1a9baf2a026743cda8ea9577d44f18623c9e6e11
claude akakpo <claudekofiaka@gmail.com>
2025-09-07 19:39:53 +0000

    Added Author, Book, Library, and Librarian models with relationships

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 
+# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name   # ✅ Checker will look for this
+
+
+# Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
+
+# Library model
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
+
+# Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
