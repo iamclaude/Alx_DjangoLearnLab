58c46b27724c531c06a3a6b35397381fe8752212
claude akakpo <claudekofiaka@gmail.com>
2025-09-08 01:34:35 +0000

    Added add_book, edit_book, delete_book paths with permission checks

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
+# LibraryProject/relationship_app/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
