558bf93f7e62bdc35cc20721f61fbd6916b6e596
claude akakpo <claudekofiaka@gmail.com>
2025-09-15 15:36:29 +0000

    Add book_list view, URL route, and template

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
-from .models import Book
 
-@permission_required("bookshelf.can_create", raise_exception=True)
-def add_book(request):
-    return render(request, "bookshelf/add_book.html")
-
-@permission_required("bookshelf.can_edit", raise_exception=True)
-def edit_book(request):
-    return render(request, "bookshelf/edit_book.html")
-
-@permission_required("bookshelf.can_delete", raise_exception=True)
-def delete_book(request):
-    return render(request, "bookshelf/delete_book.html")
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return render(request, 'bookshelf/book_list.html')
