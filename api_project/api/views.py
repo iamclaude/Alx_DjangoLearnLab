dc2f1a802dafe1f31ad87189e2e34252c7e45c82
claude akakpo <claudekofiaka@gmail.com>
2025-09-15 19:39:57 +0000

    Add BookList view extending generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,7 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
