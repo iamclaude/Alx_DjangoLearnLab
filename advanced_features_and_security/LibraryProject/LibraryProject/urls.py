15e46b5e43425a1a7fde95c392fed4aa867a68ea
claude akakpo <claudekofiaka@gmail.com>
2025-09-15 15:14:52 +0000

    Add book_list view with permission checks and template

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("bookshelf.urls")),
     path('bookshelf/', include('bookshelf.urls', namespace='bookshelf')),
     path('', include('relationship_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from . import views
-
-app_name = 'bookshelf'
+from .views import book_list
 
 urlpatterns = [
-    path('', views.list_books, name='list_books'),
-    path('create/', views.create_book, name='create_book'),
-    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path("books/", book_list, name="book_list"),
 ]
