65b5218f238bb02bfa3b2cbe5d64f086df216357
claude akakpo <claudekofiaka@gmail.com>
2025-10-07 13:11:32 +0000

    Added login_required import and ensured LoginRequiredMixin/UserPassesTestMixin

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 
